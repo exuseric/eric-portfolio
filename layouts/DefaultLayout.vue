0eb7acbc370e7c7f821852989f2c3fb3d30f7702
Eric Maina <frac710n5@gmail.com>
2022-01-04 14:50:36 +0300

    Set the default display mode (dark mode)

--- layouts/DefaultLayout.vue
+++ layouts/DefaultLayout.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="default-layout">
+  <div :class="`default-layout ${defaultDisplayMode}`">
     <Nuxt />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      defaultDisplayMode: 'dm',
+    }
+  },
+  beforeMount() {
+    this.setDisplayModeInLocalStorage()
+  },
+  methods: {
+    setDisplayModeInLocalStorage() {
+      if (localStorage.getItem('displayMode') === null) {
+        localStorage.setItem(
+          'displayMode',
+          JSON.stringify(this.defaultDisplayMode)
+        )
+      }
+    },
+  },
+}
+</script>

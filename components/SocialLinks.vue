135befa8d629b89dd25bd74e1fadf04c71016a0d
Eric Maina <frac710n5@gmail.com>
2022-01-05 11:48:42 +0300

    Add social links

---
+++ components/SocialLinks.vue
@@ -0,0 +1,12 @@
+<template>
+  <ul class="socials">
+    <li>
+      <a
+        href="http://github.com/exuseric"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Github</a
+      >
+    </li>
+  </ul>
+</template>

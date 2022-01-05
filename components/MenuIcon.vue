185789a18071e23e7fef6da047e7d757dae04dac
Eric Maina <frac710n5@gmail.com>
2022-01-05 11:47:04 +0300

    Create a hamburger icon component

---
+++ components/MenuIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg
+    version="1.1"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 100 100"
+    fill="currentColor"
+  >
+    <path
+      d="M 50 -2.842170943040401e-15 L 50 25 L 2.842170943040401e-15 50 L 0 25.000000000000004Z"
+    ></path>
+    <path d="M 50 50 L 50 75 L 2.842170943040401e-15 100 L 0 75Z"></path>
+    <path
+      d="M 50.00000000000001 50.00000000000001 L 50 25.000000000000007 L 100 -5.684341886080802e-15 L 100 24.999999999999996Z"
+    ></path>
+    <path d="M 50.00000000000001 100 L 50 75 L 100 50 L 100 75Z"></path>
+  </svg>
+</template>
+
+<style scoped>
+svg {
+  width: 100%;
+  height: 100%;
+}
+</style>

1c1a623832cf00dae93aaf855bd46f5dd4bc4934
Eric Maina <frac710n5@gmail.com>
2022-01-05 11:47:53 +0300

    Add button to toggle menu in small screens

---
+++ components/MenuToggleButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="toggle-menu center"><MenuIcon /></button>
+</template>
+
+<style scoped>
+.toggle-menu {
+  width: var(--icon-wh);
+  height: var(--icon-wh);
+
+  color: var(--white);
+  background-color: var(--primary-dark);
+
+  border: 2px solid transparent;
+}
+</style>

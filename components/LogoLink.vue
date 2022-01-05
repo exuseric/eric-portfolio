3e5402f05a15095612a6801426657818475ed761
Eric Maina <frac710n5@gmail.com>
2022-01-05 11:46:41 +0300

    Create logo component

---
+++ components/LogoLink.vue
@@ -0,0 +1,20 @@
+<template>
+  <nuxt-link to="/" class="logo-link">
+    <img src="~/assets/img/logo/dark.png" alt="Eric's Logo" />
+  </nuxt-link>
+</template>
+
+<style scoped>
+.logo-link {
+  display: block;
+
+  width: 3.5rem;
+  height: 3.5rem;
+
+  overflow: hidden;
+}
+
+.logo-link img {
+  height: 100%;
+}
+</style>

39b8602faa20da9eee5759292ae3a37fb59b5df5
Eric Maina <ericbro29@gmail.com>
2022-02-06 18:18:59 +0300

    Add the projects sections on the homepage/mainpage

---
+++ components/IndexPage/ProjectSection.vue
@@ -0,0 +1,76 @@
+<template>
+  <section class="projects">
+    <div class="heading">
+      <h2>Project Showcase</h2>
+
+      <div class="links">
+        <a
+          href="https://github.com/exuseric"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="link"
+          >on github</a
+        >
+        <a
+          href="https://github.com/exuseric"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="link"
+          >on behance</a
+        >
+      </div>
+    </div>
+
+    <div class="project-grid">
+      <ProjectCard />
+    </div>
+  </section>
+</template>
+
+<script>
+import ProjectCard from './ProjectCard.vue'
+
+export default {
+  components: { ProjectCard },
+}
+</script>
+
+<style lang="scss" scoped>
+.projects {
+  .heading {
+    @include center;
+    padding: $pd-l;
+  }
+
+  h2 {
+    font-size: scale('h2');
+    font-family: family('heading');
+    font-weight: 700;
+    color: color('dark');
+    line-height: 1.25;
+
+    text-align: center;
+    padding: $pd-m;
+  }
+
+  .link {
+    @include center;
+    width: auto;
+    height: $box-btn-h;
+
+    text-decoration: none;
+    text-transform: capitalize;
+    color: color('accent');
+  }
+}
+
+.links {
+  @include grid-flow($flow: column, $gap: 0.625rem);
+  justify-content: space-between;
+}
+
+.project-grid {
+  @include grid-row($row-count: auto, $row-height: 1fr, $gap: 5rem);
+  padding: 5rem 0;
+}
+</style>

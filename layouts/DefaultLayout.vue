6c975f9c7edb02911cc2e288422bdb5b7de370a4
Eric Maina <ericbro29@gmail.com>
2022-02-06 18:17:42 +0300

    Add logic for dynamic grid layout configuration

--- layouts/DefaultLayout.vue
+++ layouts/DefaultLayout.vue
@@ -1,10 +1,53 @@
 <template>
-  <div class="default-layout">
+  <div ref="layout" class="default-layout">
     <!-- <PageNavigation /> -->
-    <div class="grid-layout">
-      <div class="container">
-        <Nuxt />
-      </div>
+
+    <div class="container">
+      <Nuxt />
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      gridTemplate: '',
+      deviceWidth: '',
+    }
+  },
+  beforeMount() {
+    this.getDeviceWidth()
+  },
+  mounted() {
+    this.setLayoutGrid()
+  },
+  methods: {
+    getDeviceWidth() {
+      this.deviceWidth = window.innerWidth
+    },
+    setLayoutGrid() {
+      const columnCount = 12
+      const columnWidth = this.deviceWidth / columnCount / 16
+
+      const gridTemplate = `repeat(${columnCount}, ${columnWidth}rem)`
+      // this.gridTemplate = gridTemplate
+      this.$refs.layout.style.display = 'grid'
+      this.$refs.layout.style.gridTemplateColumns = gridTemplate
+    },
+    resetGrid() {
+      this.getDeviceWidth()
+      this.setLayoutGrid()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  grid-column: 1 / -1;
+  @include large-screen {
+    grid-column: 2 / -2;
+  }
+}
+</style>

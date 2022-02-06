1de1010c38044cc393784fd95be8cdae835aaa53
Eric Maina <ericbro29@gmail.com>
2022-02-06 18:20:32 +0300

    Add the homepage layout components

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,22 @@
 <template>
-  <HeroHeader />
+  <section class="home page-wrapper">
+    <HeroHeader />
+    <ProjectSection />
+    <OutroSection />
+  </section>
 </template>
 
 <script>
+import HeroHeader from '~/components/IndexPage/HeroHeader.vue'
+import OutroSection from '~/components/IndexPage/OutroSection.vue'
+import ProjectSection from '~/components/IndexPage/ProjectSection.vue'
 export default {
   name: 'IndexPage',
+  components: {
+    HeroHeader,
+    OutroSection,
+    ProjectSection,
+  },
   layout: 'DefaultLayout',
 }
 </script>

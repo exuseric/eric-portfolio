25b141baed94aa9bdbbfc85841aa3edcec7f1fdb
Eric Maina <ericbro29@gmail.com>
2022-02-08 15:10:01 +0300

    Create the logo component for the main navigation

---
+++ components/PageNavigation/LogoLink.vue
@@ -0,0 +1,38 @@
+<template>
+  <nuxt-link to="/" class="logo-link">
+    <img src="~/assets/img/logo/dark.png" alt="Eric's Logo" />
+    <span class="txt">Eric Maina</span>
+  </nuxt-link>
+</template>
+<script>
+export default {
+  name: 'LogoLink',
+}
+</script>
+<style lang="scss" scoped>
+.logo-link {
+  @include grid-flow($flow: column, $gap: 0.625rem);
+
+  width: fit-content;
+  height: $icon-wh;
+
+  overflow: hidden;
+
+  text-transform: capitalize;
+  text-decoration: none;
+
+  .txt {
+    font-size: scale('body');
+    color: color('dark');
+    align-self: end;
+  }
+}
+
+.logo-link img {
+  width: $icon-wh;
+  height: $icon-wh;
+
+  object-fit: contain;
+  object-position: center;
+}
+</style>

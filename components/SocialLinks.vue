9f0d39b43a4a47f81a10ea18604c22c39e9fe915
Eric Maina <ericbro29@gmail.com>
2022-02-13 11:45:37 +0300

    Add the linkedin social link

--- components/SocialLinks.vue
+++ components/SocialLinks.vue
@@ -6,23 +6,27 @@
     <li>
       <twitter-icon-link />
     </li>
+    <li>
+      <linked-in-icon-link />
+    </li>
   </ul>
 </template>
 
 <script>
 import GithubIconLink from '~/components/Icons/GithubIconLink.vue'
 import TwitterIconLink from '~/components/Icons/TwitterIconLink.vue'
+import LinkedInIconLink from '~/components/Icons/LinkedInIconLink.vue'
 
 export default {
   name: 'SocialLinks',
-  components: { GithubIconLink, TwitterIconLink },
+  components: { GithubIconLink, TwitterIconLink, LinkedInIconLink },
 }
 </script>
 
 <style lang="scss" scoped>
 .socials {
   @include grid-flow($flow: column, $gap: 0.625rem);
-  justify-content: start;
+  justify-content: inherit;
 
   width: 100%;
   height: 100%;

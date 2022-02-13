c8fd0a3b7be403f6e90ac0472b142c2371b0f4bb
Eric Maina <ericbro29@gmail.com>
2022-02-13 12:25:56 +0300

    Create about page

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="about page-wrapper"></section>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+  layout: 'DefaultLayout',
+}
+</script>
+
+<style lang="scss" scoped></style>

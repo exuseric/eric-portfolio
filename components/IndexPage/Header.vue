79248418e8a6e8c7bc34bcd60f6800b1c2afda48
Eric Maina <ericbro29@gmail.com>
2022-03-15 09:23:17 +0300

    Add email link

--- components/IndexPage/Header.vue
+++ components/IndexPage/Header.vue
@@ -2,15 +2,21 @@
   <header class="hero">
     <div class="heading">
       <h1 class="desc">
-        Hello <span aria-label="vulcan greeting emoji">🖖</span>, I'm Eric and I
-        am a frontend developer;
-        <p class="large">I build resilient and inclusive user interfaces.</p>
+        <p class="mid">Eric Maina.</p>
+        <p class="large">
+          I build resilient and inclusive user interfaces for the web.
+        </p>
       </h1>
     </div>
+    <div class="contact">
+      <nuxt-link to="/contact" class="contact__btn primary-btn">
+        Let's Work Together
+      </nuxt-link>
 
-    <nuxt-link to="/contact" class="contact-btn primary-btn"
-      >Let's Work Together</nuxt-link
-    >
+      <a href="mailto:ericmaina.gathoni@gmail.com" class="contact__email">
+        ericmaina.gathoni@gmail.com
+      </a>
+    </div>
   </header>
 </template>
 
@@ -23,35 +29,69 @@ export default {
 <style lang="scss" scoped>
 .hero {
   @include center;
-  height: 50rem;
+  height: 40rem;
 }
 
 .heading {
   height: 100%;
   width: 100%;
-  padding: $pd-l $pd-m;
+  padding: $pd-m;
 
   h1 {
-    font-size: scale('h6');
-    color: color('mc');
-    font-weight: 400;
+    .mid {
+      font-size: scale('h4');
+    }
 
     .large {
-      font-family: family('heading');
+      font-family: family('small cap');
       font-size: scale('h1');
-      color: color('accent');
-      padding: $pd-m 0;
+      color: primary('500');
+      padding: $pd-s 0;
 
       line-height: 1.125;
     }
   }
 }
 
-.contact-btn {
-  justify-self: center;
+.contact {
+  @include grid-column(
+    $col-count: auto-fit,
+    $col-width: minmax(12.5rem, 1fr),
+    $gap: $pd-m
+  );
+  height: auto;
+  width: 25rem;
+
+  padding: $pd-m;
 
   @include mid-screen {
-    justify-self: start;
+    width: 40rem;
   }
+}
+
+.contact__btn {
+  // justify-self: center;
+  color: neutral('50');
+  background-color: neutral('950');
+}
+
+.contact__email {
+  @include center;
+  position: relative;
+
+  width: $box-btn-w;
+  height: $box-btn-h;
+
+  color: neutral('950');
+  text-decoration: none;
+
+  transition: all $transition;
+
+  border: 2px solid currentColor;
+}
+
+.contact__email:hover {
+  color: neutral('50');
+  background-color: neutral('950');
 }
 </style>

fa5975a1170c4ab05171f77e0c8b532d25b53515
Eric Maina <70095144+exuseric@users.noreply.github.com>
2022-05-28 22:26:16 +0300

    Add contact page

---
+++ components/ContactPage/header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="header">
+    <div class="wrapper">
+      <h1>Work Inquiry, Curious Visitor or Just To Say Hi!</h1>
+      <p>Leave a message.</p>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'ContactPageHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  @include center;
+  height: 100vh;
+
+  .wrapper {
+    @include center;
+    height: 40rem;
+    padding: 0 $spacing-md;
+
+    background-image: url('~assets/img/illustration/sticking tongue out.png');
+    background-position: bottom right;
+    background-repeat: no-repeat;
+    background-size: rem(250);
+    h1 {
+      font-size: scale('h1');
+      font-family: $heading;
+      font-weight: 400;
+      color: $primary-500;
+    }
+
+    p {
+      font-size: scale('h3');
+      font-weight: 900;
+      margin: auto;
+
+      text-transform: uppercase;
+    }
+  }
+}
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="contact page-wrapper">
+    <ContactPageHeader />
+    <ContactForm />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ContactPage',
+  layout: 'DefaultLayout',
+}
+</script>
+
+<style lang="scss" scoped></style>

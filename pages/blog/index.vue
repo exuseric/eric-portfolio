23f7296c9b2bcaa418f003fe6ba707097964a895
Eric Maina <70095144+exuseric@users.noreply.github.com>
2022-08-01 14:29:55 +0300

    add blog page

---
+++ pages/blog/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="blog">
+    <header>
+      <div class="container">
+        <h1>UNDER CONSTRUCTION</h1>
+        <nuxt-link to="/" class="primary-btn" role="button">Go Home</nuxt-link>
+      </div>
+    </header>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  place-content: center;
+
+  margin-top: $navigation-height;
+  padding-top: $spacer-lg;
+  height: calc(100vh - $navigation-height);
+
+  text-align: center;
+
+  color: $light-gray;
+
+  @include light-theme {
+    color: $dark-gray;
+  }
+
+  h1 {
+    font-size: scale(h1);
+  }
+}
+</style>
